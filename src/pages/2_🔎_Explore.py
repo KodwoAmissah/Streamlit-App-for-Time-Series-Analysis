0663934276cbded390178a50b98aa87c50e91378
Kodwo Amissah-Mensah <kodwoam@gmail.com>
2023-09-24 19:18:00 +0000

    add explore page

---
+++ src/pages/2_🔎_Explore.py
@@ -0,0 +1,24 @@
+##import libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+##define functions
+def load_data(path):
+    dataset=pd.read_csv("Notebook\\train_data.csv")
+    return dataset
+
+#load the dataset
+r_path="Notebook\\train_data.csv"
+load_df=load_data(r_path)
+
+#Define section
+data=st.container()
+
+##set up data section tht users will interact with
+with data:
+    data.write("On this page,you can preview the dataset")
+
+
+    if data.button("Preview the dataset"):
+        data.write(load_df)
